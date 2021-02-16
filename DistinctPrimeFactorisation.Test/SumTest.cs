af2a74a8d0f6f3fc95069c820b776fdf102bb325
NickWingate <nicholas.wingate03@gmail.com>
2021-02-16 15:32:48 +0000

    Adds unit test for sum

--- DistinctPrimeFactorisation.Test/SumTest.cs
+++ DistinctPrimeFactorisation.Test/SumTest.cs
@@ -8,10 +8,21 @@ namespace DistinctPrimeFactorisation.Test
 {
     public class SumTest
     {
-        [Test]
-        public void SumTwoNumbers()
+        [TestCase(new int[2] { 1, 2 }, 3)]
+        [TestCase(new int[2] { 50, 23 }, 73)]
+        [TestCase(new int[2] { -1, -3 }, -4)]
+        [TestCase(new int[2] { 50, -20 }, 30)]
+        public void SumTwoNumbers(int[] input, int result)
         {
-            Assert.AreEqual(3, Program.Sum(new List<int> { 1, 2 }));
+            Assert.AreEqual(result, Program.Sum(input));
+        }
+
+        [TestCase(new int[] { 1, 2, 3, 4 }, 10)]
+        [TestCase(new int[] { 4, 3, 1, 2 }, 10)]
+        [TestCase(new int[] { 10, -50, 2, 37 }, -1)]
+        public void SumSeveralNumbers(int[] input, int result)
+        {
+            Assert.AreEqual(result, Program.Sum(input));
         }
     }
 }
