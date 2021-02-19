3b5431cfdc8a6b6d8b1cae3717f80949cb36a402
NickWingate <nicholas.wingate03@gmail.com>
2021-02-19 14:19:02 +0000

    Adds another test for FindPrimeFactorsTest.cs
    Signed-off-by: NickWingate <nicholas.wingate03@gmail.com>

--- DistinctPrimeFactorisation.Test/FindPrimeFactorsTest.cs
+++ DistinctPrimeFactorisation.Test/FindPrimeFactorsTest.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace DistinctPrimeFactorisation.Test
 {
@@ -12,9 +13,19 @@ namespace DistinctPrimeFactorisation.Test
         [TestCase(100, new int[] { 2,2,5,5 })]
         [TestCase(32, new int[] { 2,2,2,2,2 })]
         [TestCase(123, new int[] { 3,41 })]
-        public void FindPrimeFactors(int input, int[] result)
+        public void FindPrimeFactors_ShouldFindPrimeFactors_WhenValidInput(int input, int[] result)
         {
             Assert.AreEqual(new List<int>(result), Program.FindPrimeFactors(input));
         }
+
+        [TestCase(3)]
+        [TestCase(5)]
+        [TestCase(41)]
+        [TestCase(23)]
+        public void FindPrimeFactors_ShouldReturnInput_WhenInputPrime(int prime)
+        {
+            Assert.AreEqual(new List<int>{ prime }, Program.FindPrimeFactors(prime));
+        }
+        
     }
 }
