9087ded2e1d42e14f72a4d232cd82d0019b48782
NickWingate <nicholas.wingate03@gmail.com>
2021-02-20 21:15:09 +0000

    Added Cards solution and Unit Test
    Signed-off-by: NickWingate <nicholas.wingate03@gmail.com>

---
+++ Cards.Test/ProgramTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Cards.Test
+{
+	public class ProgramTests
+	{
+		[Test]
+		public void CountCardFrequency_ShouldCountFrequency_WhenInputValid()
+		{
+			// Arrange
+			var cards = new int[] { 1, 1, 3, 5, 6 };
+			var expected = new Dictionary<int, int>()
+			{
+				{1, 2},
+				{3, 1},
+				{5, 1},
+				{6, 1},
+			};
+			// Act
+
+			// Assert
+			Assert.AreEqual(expected, Program.CountCardFrequency(cards));
+		}
+
+		[TestCase(new int[] {1, 1}, 1)]
+		[TestCase(new int[] {1, 1, 1}, 3)]
+		[TestCase(new int[] {1, 1, 1, 2, 2}, 4)]
+		[TestCase(new int[] {1, 2, 1, 2, 1}, 4)]
+		[TestCase(new int[] {1, 2, 1, 2, 3}, 2)]
+		[TestCase(new int[] {5, 5, 10, 5, 5}, 6)]
+		public void FindGroups_ShouldCountPossiblePairs_WhenInputValid(int[] cards, int result)
+		{
+			Assert.AreEqual(result, Program.FindGroups(cards, 2));
+		}
+
+		[TestCase(new int[] { 1, 1 }, 2, 1)]
+		[TestCase(new int[] { 1, 1 }, 1, 2)]
+		[TestCase(new int[] { 1, 1, 2, 6 }, 10, 1)]
+		public void NumberOfSums_ShouldReturnNumberOfWaysToMeetGoal(
+			int[] numbersInput, int goalInput, int expectedResult)
+		{
+			Assert.AreEqual(expectedResult, 
+				Program.NumberOfSums(numbersInput, goalInput));
+		}
+	}
+}
---
+++ Cards/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Combinatorics.Collections;
+
+namespace Cards
+{
+	public static class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				int[] cards = GetUserCards();
+				int score = FindGroups(cards, 2);
+				score += NumberOfSums(cards, 15);
+				Console.WriteLine(score);
+			}
+
+		}
+
+		public static int NumberOfSums(int[] numbers, int goal)
+		{
+			int sums = 0;
+			for (int i = 1; i <= numbers.Length; i++)
+			{
+				var combinations = new Combinations<int>(numbers, i);
+				foreach (var combination in combinations)
+				{
+					if (combination.Sum() == goal)
+					{
+						sums++;
+					}
+				}
+			}
+
+			return sums;
+		}
+
+		public static int FindGroups(int[] cards, int groupSize)
+		{
+			var cardFrequencyTable = CountCardFrequency(cards);
+			var pairCount = 0;
+			foreach (var frequency in cardFrequencyTable.Values)
+			{
+				// nCr = n! / r! * (n - r)!
+				pairCount += Factorial(frequency) /
+					(Factorial(groupSize) * Factorial(frequency - groupSize));
+			}
+
+			return pairCount;
+		}
+		
+		private static int Factorial(int i)
+		{
+			if (i <= 1)
+			{
+				return 1;
+			}
+
+			return i * Factorial(i - 1);
+		}
+
+		public static Dictionary<int,int> CountCardFrequency(int[] cards)
+		{
+			Dictionary<int, int> CardFrequency = new Dictionary<int, int>();
+			foreach (var card in cards)
+			{
+				if (CardFrequency.ContainsKey(card))
+				{
+					CardFrequency[card]++;
+				}
+				else
+				{
+					CardFrequency.Add(card, 1);
+				}
+			}
+
+			return CardFrequency;
+		}
+
+		private static int[] GetUserCards()
+		{
+			Console.WriteLine("Enter your cards seperated by spaces");
+			string rawInput = Console.ReadLine();
+			string[] rawCards = rawInput.Split(' ', StringSplitOptions.TrimEntries);
+			int[] cards = new int[rawCards.Length];
+			for (int i = 0; i < rawCards.Length; i++)
+			{
+				cards[i] = Convert.ToInt32(rawCards[i]);
+			}
+
+			return cards;
+		}
+	}
+}
